52789dc3f4e1188c8827861134785a0df43c6e58
Zibi Braniecki <gandalf@mozilla.com>
2018-07-30 16:21:46 -0700

    Switch touchNext to also store promises

--- src/cached_async_iterable.mjs
+++ src/cached_async_iterable.mjs
@@ -26,26 +26,6 @@ export default class CachedAsyncIterable extends CachedIterable {
     }
 
     /**
-     * Synchronous iterator over the cached elements.
-     *
-     * Return a generator object implementing the iterator protocol over the
-     * cached elements of the original (async or sync) iterable.
-     */
-    [Symbol.iterator]() {
-        const cached = this;
-        let cur = 0;
-
-        return {
-            next() {
-                if (cached.length === cur) {
-                    return {value: undefined, done: true};
-                }
-                return cached[cur++];
-            }
-        };
-    }
-
-    /**
      * Asynchronous iterator caching the yielded elements.
      *
      * Elements yielded by the original iterable will be cached and available
@@ -62,7 +42,7 @@ export default class CachedAsyncIterable extends CachedIterable {
                 if (cached.length <= cur) {
                     cached.push(cached.iterator.next());
                 }
-                return await cached[cur++];
+                return cached[cur++];
             }
         };
     }
@@ -77,10 +57,10 @@ export default class CachedAsyncIterable extends CachedIterable {
         let idx = 0;
         while (idx++ < count) {
             const last = this[this.length - 1];
-            if (last && last.done) {
+            if (last && (await last).done) {
                 break;
             }
-            this.push(await this.iterator.next());
+            this.push(this.iterator.next());
         }
         // Return the last cached {value, done} object to allow the calling
         // code to decide if it needs to call touchNext again.
